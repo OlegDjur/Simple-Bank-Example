96d1c014c164983a8e217374e9ce0ea1f391d3bb
Oleg_Dzhur <olegdjur@gmail.com>
2023-01-31 13:22:58 +0600

    added unit test for a function GetAccount in the repository layer

--- internal/repository/account_test.go
+++ internal/repository/account_test.go
@@ -3,13 +3,19 @@ package repository
 import (
 	"context"
 	"sbank/internal/controller/dto"
+	"sbank/internal/models"
 	"sbank/internal/utils"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func createRandomAccount(t *testing.T) models.Account {
 	arg := dto.CreateAccountDTO{
 		Owner:    utils.RandomOwner(),
 		Balance:  utils.RandomMoney(),
@@ -27,4 +33,20 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testRepository.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
