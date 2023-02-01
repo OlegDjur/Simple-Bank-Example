57869e1df4d5d29f47c625bdfefb09c4c2ab4b2c
Oleg_Dzhur <olegdjur@gmail.com>
2023-02-01 14:07:23 +0600

    created func listTransfers in repository layer

--- internal/controller/dto/transfer.go
+++ internal/controller/dto/transfer.go
@@ -11,3 +11,10 @@ type TransferTxDTO struct {
 	ToAccountID   int64 `json:"to_account_id"`
 	Amount        int64 `json:"amount"`
 }
+
+type ListTransfersDTO struct {
+	FromAccountID int64 `json:"from_acount_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Limit         int32 `json:"limit"`
+	Offset        int32 `json:"offset"`
+}
--- internal/repository/transfer.go
+++ internal/repository/transfer.go
@@ -9,6 +9,7 @@ import (
 
 type transfer interface {
 	CraeteTransfer(ctx context.Context, arg dto.TransferDTO) (models.Transfer, error)
+	GetTransfer(ctx context.Context, id int64) (models.Transfer, error)
 }
 
 type TransferStorage struct {
@@ -57,4 +58,46 @@ func (ts *TransferStorage) GetTransfer(ctx context.Context, id int64) (models.Tr
 	)
 
 	return transfer, err
+}
+
+func (ts *TransferStorage) ListTransfers(ctx context.Context, arg dto.ListTransfersDTO) ([]models.Transfer, error) {
+	query := `SELECT id, from_account_id, to_account_id, created_at FROM transfers 
+		WHERE 
+			from_account_id = $1 OR 
+			to_account_id = 2,
+		ORDER BY id
+		LIMIT $3
+		OFFSET $4
+	`
+
+	rows, err := ts.db.QueryContext(ctx, query, arg.FromAccountID, arg.ToAccountID, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	items := []models.Transfer{}
+
+	for rows.Next() {
+		var i models.Transfer
+		if err := rows.Scan(
+			&i.ID,
+			&i.FromAccountID,
+			&i.ToAccountID,
+			&i.Amount,
+			&i.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return items, nil
 }
